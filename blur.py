6735fd690a83d365cdb486dbf6ab34bea5d17c8d
vihrdaya <kevin.rajkumar@outlook.com>
2022-07-30 10:28:33 -0400

    Fixed bug with croppedim not being defined.

--- blur.py
+++ blur.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2 as cv
-from scipy.ndimage import filters
+from scipy.ndimage import gaussian_filter
 #from pylab import *
 
 video = cv.VideoCapture(0)
@@ -11,18 +11,15 @@ face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 while True:
 
     ret, frame = video.read()
-
-    #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(frame, 1.3, 5)
-
+    
     for (x, y, w, h) in faces:
-        #        cv.rectangle(frame,(x,y),(x+w, y+h),(255, 0, 0), 2)
+#       cv.rectangle(frame,(x,y),(x+w, y+h),(255, 0, 0), 2)
         roi_color = frame[y:y+h, x:x+w]
         box = frame[y:y+h, x:x+w]
         croppedim = np.zeros(roi_color.shape)
         for i in range(3):
-            croppedim[:, :, i] = filters.gaussian_filter(
-                roi_color[:, :, i], 20)
+            croppedim[:, :, i] = gaussian_filter(roi_color[:, :, i], 20)
 
     frame[y:y+h, x:x+w] = croppedim
 
